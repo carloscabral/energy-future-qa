c1f6bb2607a60288c00b93d35328dc9636dc4fab
Carlos Gomes Cabral <carlos.gomescabral@gmail.com>
2019-11-02 16:48:11 -0300

    Fixed form fields

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -18,15 +18,15 @@
             <label class="subscription-form__label" for="name">Nome</label>
             <input class="subscription-form__input" type="text" name="name" placeholder="Digite seu primeiro nome" v-model="formData.name" required="true" />
             
-            <label class="subscription-form__label" for="last-name">Sobrenome</label>
-            <input class="subscription-form__input" type="text" name="last-name" placeholder="Digite seu sobrenome" v-model="formData.lastName" required="true" />
+            <label class="subscription-form__label" for="lastName">Sobrenome</label>
+            <input class="subscription-form__input" type="text" name="lastName" placeholder="Digite seu sobrenome" v-model="formData.lastName" required="true" />
             
             <label class="subscription-form__label" for="email">E-mail</label>
             <input class="subscription-form__input" type="email" name="email" placeholder="Digite seu melhor e-mail" v-model="formData.email" required="true"/>
 
             <div class="subscription-form__checkbox">
                 <label>
-                    <input type="checkbox" id="checkbox" name="checkbox" v-model="formData.checkbox" required="true" />
+                    <input type="checkbox" id="checkbox" name="newsletter" v-model="formData.newsletter" required="true" />
                     <span></span>
                     &nbsp;Eu concordo em receber e-mails com atualizações sobre o Energy Future.
                 </label>
